86ae69b21eede42be6def3d5407b51c9202afb82
Derek Merryweather <merryweatherderek@gmail.com>
2023-10-31 12:08:50 -0700

    getting rid of sliding window for slope to spike and making it just a single numeric value

--- R/spike_slope.R
+++ R/spike_slope.R
@@ -2,51 +2,35 @@
 #' 
 #' @author Derek Merryweather
 #' 
-#' @description This function 
+#' @description This function is used to find the slope to first spike in rheobase trace
 #'
 #' @param x data frame containing a time column, voltage trace column(s), and current trace column(s).
 #' @param ap_threshold threshold voltage for detecting action potential. default is -20mV.
 #' @param iStep_window indices representing the entire current step. defaults to ~650ms - 1250ms window, which is Derek's default. adjust your window accordingly.
 #' @param baseline_window indices representing the baseline window that is used for calculation. defaults to first 600ms of each sweep, which is Derek's default. adjust your window accordingly.
-#' @param sliding_window how much time the rolling window covers in ms
 #'
 #' @return returns array of slope values from start of current step to first action potential in rheobase trace
 #' 
 #' @export
 #'
 #' 
-spike_slope <- function(x, ap_threshold=-20, iStep_window=6564:11561, baseline_window=1:6000, sliding_window=0.5) {
+spike_slope <- function(x, ap_threshold=-20, iStep_window=6564:11561, baseline_window=1:6000) {
   
   #these variables are vectors containing index of voltage trace columns and current trace columns in data frame x
   vTrace_index <- c(2:(((ncol(x)-1)/2)+1))
   iTrace_index <- c((((ncol(x)-1)/2)+2):ncol(x))
   
-  #converting sliding window units to 10kHz
-  sliding_window <- sliding_window*10
-  sliding_window2 <- sliding_window
-  
   a <- x[iStep_window,vTrace_index]
   aa <- which(a >= ap_threshold, arr.ind = TRUE)
-  slp_array <- c()
+  
   if (is.na(aa[1]) == TRUE) {
-    result <- data.frame(matrix(ncol = 1,nrow = 1))
-    result$slp_array <- NA
-    result$time_ms <- NA
-    result <- result[,-c(1)]
+    result <- NA
   }
   else {
-    aaa <- aa[1,2] #getting rheobase column
-    j <- a[1:aa[1,1],aaa]
-    remainder <- length(j)%%sliding_window
-    count <- 1
-    for (i in 1:((length(j)-remainder)/sliding_window)) {
-      slp_array[i] <- (j[sliding_window2] - j[count]) / (sliding_window/10)
-      count <- count + sliding_window
-      sliding_window2 <- sliding_window2+sliding_window
-    }
-    result <- data.frame(slp_array)
-    result$time_ms <- seq(from=0, to=(length(j)-remainder)-sliding_window, by=sliding_window)
-    result$time_ms <- result$time_ms/10
+    rheoCol <- aa[1,2] #getting rheobase column
+    preSpike <- aa[1,1]-3
+    j <- a[1:preSpike,rheoCol]
+    result <- (j[length(j)] - j[1]) / (length(j)/10)
   }
   
   return(result)
