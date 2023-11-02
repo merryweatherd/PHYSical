ef39808d2657a45a2737a924a8a9bd2bb51b1beb
Derek Merryweather <merryweatherderek@gmail.com>
2023-11-02 16:35:54 -0700

    removing user defined step argument

--- R/interSpikeInterval.R
+++ R/interSpikeInterval.R
@@ -5,7 +5,6 @@
 #' @description finds the inter spike interval for user defined current step
 #'
 #' @param x data frame containing a time column, voltage trace column(s), and current trace column(s).
-#' @param step user defined current step to calculate ISI
 #' @param ap_threshold threshold voltage for detecting action potential. default is -20mV.
 #' @param iStep_window indices representing the entire current step. defaults to ~650ms - 1250ms window, which is Derek's default. adjust your window accordingly.
 #' @param baseline_window indices representing the baseline window that is used for calculation. defaults to first 600ms of each sweep, which is Derek's default. adjust your window accordingly.
@@ -14,24 +13,11 @@
 #' 
 #' @export
 #'
-interSpikeInterval <- function(x, step, ap_threshold=-20, iStep_window=6564:11561, baseline_window=1:6000) {
+interSpikeInterval <- function(x, ap_threshold=-20, iStep_window=6564:11561, baseline_window=1:6000) {
   
   #these variables are vectors containing index of voltage trace columns and current trace columns in data frame x
   vTrace_index <- c(2:(((ncol(x)-1)/2)+1))
   iTrace_index <- c((((ncol(x)-1)/2)+2):ncol(x))
-  
-  #finding user defined current injection column
-  #because i might switch up how many current steps I do and the delta increase with each current step i.e. 20pA current steps vs. 10pA current steps
-  #switching this up will change the number of columns and therefore the index for where the user defined column is. these lines account for this issue
-  #finds median value of each column's iStep_window and finds which column (trace) matches the user defined current trace
-  #have to add one at end because time is always first column
-  #importantly, this stepIndex alone should be used to access the voltage column from x, not the current column. must do iTrace_index[stepIndex-1] to get current column
-  stepIndex <- which(round(matrixStats::colMedians(as.matrix(x[iStep_window,iTrace_index])) - 
-                             matrixStats::colMedians(as.matrix(x[baseline_window,iTrace_index])),
-                           digits = 0) >= (step-4) &
-                       round(matrixStats::colMedians(as.matrix(x[iStep_window,iTrace_index])) -
-                               matrixStats::colMedians(as.matrix(x[baseline_window,iTrace_index])),
-                             digits = 0) <= (step+4)) +1
   
   a <- x[iStep_window, vTrace_index]
   aa <- which(a >= ap_threshold, arr.ind = TRUE)
